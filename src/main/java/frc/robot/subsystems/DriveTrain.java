79693e5598ba4e82329ae9b1b0060afe8ef9f2d8
R. Kyle Dunlop <rkdunlop12@gmail.com>
2019-01-28 13:35:15 -0500

    updated drvie

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -45,4 +45,10 @@ public class DriveTrain extends Subsystem {
     // Set the default command for a subsystem here.
     // setDefaultCommand(new MySpecialCommand());
   }
+
+  public void drive(double x, double rot, boolean isQuickTurn) {
+
+    m_drive.curvatureDrive(x, rot, isQuickTurn);
+
+  }
 }
